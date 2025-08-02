d49b58fbaf0c3d51c745b3080c1defeef2982b66
Lalit Meena <152899853+lalit2222@users.noreply.github.com>
2025-08-02 10:14:33 +0530

    Update db.js

--- day-5/src/db/db.js
+++ day-5/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 function connectedDb(){
-    mongoose.connect("mongodb+srv://lalitmeena9329:M6GffUVUEcKwM3rr@cluster0.in5lubj.mongodb.net/lalit").then(()=>{
+    mongoose.connect("loclhost://").then(()=>{
         console.log("connect db sucessfully");
         
     })
